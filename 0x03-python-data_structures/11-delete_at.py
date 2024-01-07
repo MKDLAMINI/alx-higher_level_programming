8ddc6d7714231d1fafc409fdb634a9f4b42b8b98
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-01-07 02:05:51 -0800

    restructured code syntax

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        del(my_list[idx])
-    return(my_list)
+    length_of_list = len(my_list)
+    if idx > length_of_list - 1:
+        return my_list
+    elif idx < 0:
+        return my_list
+    del my_list[idx]
+    return my_list
