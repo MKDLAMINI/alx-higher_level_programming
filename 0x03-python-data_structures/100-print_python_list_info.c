561dedef1d05112cd753cd8b7df08e102c71a4aa
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-01-08 04:09:38 -0800

    restructured conde syntax

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,12 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
+#include <time.h>
+
+/**
+ * print_python_list_info - prints python lists
+ * @p: PyObject list.
+ */
 
 void print_python_list_info(PyObject *p)
 {
@@ -8,7 +14,13 @@ void print_python_list_info(PyObject *p)
 	int i;
 	PyListObject *obj = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = li\n", size);
+	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i - 0; i < size; i++)
+
+	struct timespec ts;
+	clock_gettime(CLOCK_REALTIME, &ts);
+	printf("[*] Current time: %ld seconds, %ld nanoseconds\n", ts.tv_sec, ts.tv_nsec);
+
+	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
