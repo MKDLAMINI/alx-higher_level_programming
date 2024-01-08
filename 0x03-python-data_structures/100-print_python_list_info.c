5d7a27f0b638b415ab98a0e4bbbc1acee1e3202c
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-01-08 04:12:32 -0800

    testing something

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,6 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
-#include <time.h>
 
 /**
  * print_python_list_info - prints python lists
@@ -16,11 +15,6 @@ void print_python_list_info(PyObject *p)
 
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
-
-	struct timespec ts;
-	clock_gettime(CLOCK_REALTIME, &ts);
-	printf("[*] Current time: %ld seconds, %ld nanoseconds\n", ts.tv_sec, ts.tv_nsec);
-
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
