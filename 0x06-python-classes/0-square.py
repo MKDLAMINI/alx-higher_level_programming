1fb0492561cf563713658479f025621f6eac934d
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-01-23 09:58:02 -0800

    tasks 0 to 6

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""new module for square"""
+
+class Square:
+    """define a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module of a square"""
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size):
+        """Class builder
+        Args:
+            size: length of side of the square
+        """
+        self.__size = size
