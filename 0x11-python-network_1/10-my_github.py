4b2e1c0104bb7c2038eab4481e95caf0ee68f9ea
MKDLAMINI <dlaminimkultra@gmail.com>
2024-04-26 10:22:37 -0700

    rectified character limit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes your GitHub credentials (username and password) and uses the GutHun API to display your id
+Write a Python script that takes your GitHub credentials
 """
 
 
