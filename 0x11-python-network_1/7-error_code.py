e32565dcb3c4a4a6002ad6e33fd950e26d22ffe3
MKDLAMINI <dlaminimkultra@gmail.com>
2024-04-26 10:25:44 -0700

    rectified logical spacing between lines

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,14 +12,15 @@ from sys import argv
 if __name__ == "__main__":
 
     if len(argv) != 2:
+
         print("Usage: ./script.py <URL>")
         exit(1)
 
     url = argv[1]
     response = get(url)
 
-
     if response.status_code >= 400:
+
         print(f"Error: {response.status_code}")
     else:
         print(response.text)
