2d8b42feada999a00405137c6f0d9b1f8f8f7527
MKDLAMINI <dlaminimkultra@gmail.com>
2024-04-26 09:10:37 -0700

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to the URL
+and displays the body of the response
+"""
+
+
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    if len(argv) != 2:
+        print("Usage: ./script.py <URL>")
+        exit(1)
+
+    url = argv[1]
+    response = get(url)
+
+
+    if response.status_code >= 400:
+        print(f"Error: {response.status_code}")
+    else:
+        print(response.text)
